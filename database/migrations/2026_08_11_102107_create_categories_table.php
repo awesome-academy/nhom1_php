bd7c777191af1436558aed19cf102265b1c41e1f
ROSEJC <trhaidong2005@gmail.com>
2026-08-13 19:28:36 +0700

    fix(migration): add softDeletes and clean up categories table

--- database/migrations/2026_08_11_102107_create_categories_table.php
+++ database/migrations/2026_08_11_102107_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,18 +13,16 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('parent_id')
-              ->nullable()
-              ->constrained('categories')
-              ->nullOnDelete();
-              
+                ->nullable()
+                ->constrained('categories')
+                ->nullOnDelete();
+
             $table->string('name', 100);
             $table->string('slug', 120)->unique();
             $table->text('description')->nullable();
-        
-            $table->timestamps();
 
-        // Đánh index theo đúng yêu cầu thiết kế
-            $table->index('parent_id');
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 
