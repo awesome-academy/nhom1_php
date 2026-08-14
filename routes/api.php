cf5666a64570f3e68fcfb4e9a4405f835819652e
Hưng Trần <hungtd036205000405@gmail.com>
2026-08-14 19:17:28 +0700

    #98912 Remove v1 from API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\V1\CartController;
+use App\Http\Controllers\Api\CartController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')
-    ->middleware('auth:sanctum')
-    ->group(function (): void {
-        Route::get('/cart', [CartController::class, 'show']);
-    });
+Route::middleware('auth:sanctum')->group(function (): void {
+    Route::get('/cart', [CartController::class, 'show']);
+});
