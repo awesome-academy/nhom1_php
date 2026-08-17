acaa5130e4d2a99bed85fea9ea4695f0f7dd5b0d
Nguyen Dai Viet Hoang <h3m1n1@gmail.com>
2026-08-18 00:46:17 +0700

    [#98923] Add admin suggestion review APIs

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\SuggestionController as AdminSuggestionController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\RatingController;
 use App\Http\Controllers\Api\SuggestionController;
-use App\Http\Controllers\Auth\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
 // Auth
@@ -19,6 +19,11 @@ Route::middleware('auth:sanctum')->group(function (): void {
 
     Route::get('/suggestions/me', [SuggestionController::class, 'index']);
     Route::post('/suggestions', [SuggestionController::class, 'store']);
+
+    Route::prefix('admin')->middleware('admin')->group(function (): void {
+        Route::get('/suggestions', [AdminSuggestionController::class, 'index']);
+        Route::put('/suggestions/{suggestion}', [AdminSuggestionController::class, 'update']);
+    });
 
     Route::get('/cart', [CartController::class, 'show']);
     Route::post('/cart/items', [CartController::class, 'storeItem']);
